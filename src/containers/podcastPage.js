d7d6157b001fd1f01af21546ff841674614fced2
Alexandre Serra Jaumot <aserrajaumot@gmail.com>
2017-02-27 16:13:13 +0100

    New promise to get the detailed data using scripts (&callbacks)

--- src/containers/podcastPage.js
+++ src/containers/podcastPage.js
@@ -1,5 +1,22 @@
-import * as itunes from '../utils/itunes'
+// import * as itunes from '../utils/itunes'
 
 export default function ({ podcastId }) {
-  return Promise.resolve(`Podcast page: ${podcastId}`)
+  return new Promise((resolve, reject) => {
+    const podcastScript = document.createElement('script')
+    podcastScript.src = `https://itunes.apple.com/lookup?id=${podcastId}&callback=podcastCallback`
+    window.podcastCallback = function ({ results }) {
+      const feedUrl = results[0].feedUrl
+      console.log(feedUrl)
+      const episodeScript = document.createElement('script')
+      console.log('feedUrl', feedUrl)
+      episodeScript.src = `${feedUrl}?callback=episodeCallback`
+      episodeScript.type = 'text/javascript'
+      document.head.appendChild(episodeScript)
+    }
+    document.head.appendChild(podcastScript)
+
+    window.episodeCallback = function () {
+      console.log('[episodeCallback]', arguments)
+    }
+  })
 }
